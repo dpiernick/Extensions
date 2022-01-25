0d5df0e7b987fe22ab8b1f03af6dcb6919926736
Dave <Piernick>
2022-01-25 03:11:20 -0500

    VC

--- App/MainViewController.swift
+++ App/MainViewController.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    var scrollView: UIScrollView = { UIScrollView(false) }()
+    var label: UILabel = { return UILabel(false, "MainVC!") }()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+        view.addSubviewWithConstraints(scrollView)
+        addTitleView()
+    }
+
+    func addTitleView() {
+        scrollView.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.leadingAnchor.constraint(greaterThanOrEqualTo: scrollView.leadingAnchor),
+            label.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            label.trailingAnchor.constraint(lessThanOrEqualTo: scrollView.trailingAnchor),
+            label.bottomAnchor.constraint(lessThanOrEqualTo: scrollView.bottomAnchor),
+            label.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor)
+        ])
+    }
+}
+
